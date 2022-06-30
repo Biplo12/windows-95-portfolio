d1af21a6f0b1b9955c6195b94cdcb0b9759f4122
Biplo12 <75808735+Biplo12@users.noreply.github.com>
2022-06-30 23:09:27 +0200

    Deployment config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Components/Styles/__globalStyles.scss";
 
-// import Taskbar from "./Components/Taskbar/Taskbar";
+import Taskbar from "./Components/Taskbar/Taskbar";
 import Desktop from "./Components/Desktop/Desktop";
 const App = () => {
   return (
